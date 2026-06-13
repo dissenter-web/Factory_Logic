2062bd8b3b590420b576c02042cd7355193f75ed
[alex-rav] <[aleksandr_radnovski@mail.ru]>
2026-06-13 15:34:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def find_fault(code):
+    return faults.get(code)
+
+def show_faults():
+    for code, description in faults.items():
+        print(f"{code} - {description}")
+
+while True:
+    print("\n1. Показать ошибки")
+    print("2. Найти ошибку")
+    print("3. Выход")
+
+    choice = input("Выберите действие: ")
+
+    if choice == "1":
+        show_faults()
+
+    elif choice == "2":
+        code = input("Введите код ошибки: ")
+        description = find_fault()
+
+        if description:
+            print(f"Ошибка найдена. Название: {description}")
+        else:
+            print("Ошибка не найдена.")
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Неизвестная команда")   
