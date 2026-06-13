f4f26a18cf0a3068d2decffbee72379d36d6a685
[alex-rav] <[aleksandr_radnovski@mail.ru]>
2026-06-13 21:41:29 +0300

    Refactor error dictionary structure

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
+
+with open("faults.json", "r", encoding="utf-8") as file:
+    faults = json.load(file)
+
 def find_fault(code):
     return faults.get(code)
 
@@ -5,10 +10,13 @@ def show_faults():
     for code, description in faults.items():
         print(f"{code} - {description}")
 
+        for inner_code, inner_description in description.items():
+            print(f"   {inner_code} - {inner_description}")
+
 while True:
     print("\n1. Показать ошибки")
     print("2. Найти ошибку")
-    print("3. Выход")
+    print("3. Выход\n")
 
     choice = input("Выберите действие: ")
 
@@ -16,8 +24,8 @@ while True:
         show_faults()
 
     elif choice == "2":
-        code = input("Введите код ошибки: ")
-        description = find_fault()
+        code = input("Введите код ошибки: ").upper()
+        description = find_fault(code)
 
         if description:
             print(f"Ошибка найдена. Название: {description}")
