2ae1a613f28c3d305817a16a9944f9641e9d13e5
Dissenter-web <[aleksandr_radnovski@mail.ru]>
2026-06-27 01:20:25 +0300

    feat: integrate MAX bot with fault service

--- app/interfaces/max_bot/max_bot.py
+++ app/interfaces/max_bot/max_bot.py
@@ -1,6 +1,9 @@
 import time
 import requests
 from app.config import MAX_BOT_TOKEN
+from app.services import faults_service
+from app.formatters.fault_formatter import format_fault, format_all_faults
+from app.repositories.json_repository import load_faults
 
 API_URL = "https://platform-api.max.ru"
 
@@ -30,7 +33,74 @@ def request_api(method, path, params=None, json=None):
 
     return {}
 
+def echo():
+    marker = None
+
+    while True:
+        data = request_api(
+            "GET", 
+            "/updates",
+            params={"marker": marker} if marker else None,
+            )
+        
+        marker = data["marker"]
+
+        updates = data["updates"]
+
+        if updates:
+            message = updates[0]["message"]
+            is_bot = message["sender"]["is_bot"]
+
+            if not is_bot:
+                message_text = message["body"]["text"]
+                chat_id = message["recipient"]["chat_id"]
+        
+                request_api(
+                        "POST",
+                        "/messages",
+                        params={"chat_id": chat_id},
+                        json={"text": message_text}
+                    )
+                
+def faults_handling():
+    faults = load_faults()
+
+    marker = None
+
+    while True:
+        data = request_api(
+            "GET", 
+            "/updates",
+            params={"marker": marker} if marker else None,
+            )
+        
+        marker = data["marker"]
+
+        updates = data["updates"]
+
+        if updates:
+            update = updates[0]
+
+            if "message" not in update:
+                continue
+
+            message = update["message"]
+            is_bot = message["sender"]["is_bot"]
+
+            if not is_bot:
+                message_text = message["body"]["text"]
+                chat_id = message["recipient"]["chat_id"]
+
+                fault_code = faults_service.normalize_fault_code(message_text)
+                fault_data = faults_service.find_fault(faults, fault_code)
+        
+                request_api(
+                        "POST",
+                        "/messages",
+                        params={"chat_id": chat_id},
+                        json={"text": format_fault(fault_code.upper(), fault_data)}
+                    )
 
 
 if __name__ == "__main__":
-    print(request_api("GET", "/updates"))
+    faults_handling()
