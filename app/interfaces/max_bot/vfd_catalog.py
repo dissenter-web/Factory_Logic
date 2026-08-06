f50e4b661eedcdd0179c096f5495931daec4e504
Dissenter-web <[aleksandr_radnovski@mail.ru]>
2026-08-06 20:04:20 +0300

    feat: create vfd_catalog

---
+++ app/interfaces/max_bot/vfd_catalog.py
@@ -0,0 +1,16 @@
+VFD_CATALOG = {
+    "allen_bradley": {
+        "name": "Allen-Bradley",
+        "menu_payload": "ab_menu",
+        "models": {
+            "pf525": {
+                "name": "PowerFlex 525",
+                "fault_example": "F005",
+            },
+            "pf40": {
+                "name": "PowerFlex 40",
+                "fault_example": "F005",
+            },
+        },
+    },
+}
